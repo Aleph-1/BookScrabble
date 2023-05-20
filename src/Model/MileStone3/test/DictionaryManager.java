18111fef6046e91bdbf3e7c6bab91215caa41192
IdoShimon <ido.shimon6@gmail.com>
2023-05-20 17:41:54 +0300

    Started working with the patam1

--- src/Model/MileStone3/test/DictionaryManager.java
+++ src/Model/MileStone3/test/DictionaryManager.java
@@ -6,7 +6,7 @@ public class DictionaryManager {
     HashMap<String,Dictionary> books;
     private static volatile DictionaryManager dm = null;
 
-    DictionaryManager(){
+    public DictionaryManager(){
         books = new HashMap<String, Dictionary>();
     }
 
---
+++ src/Model/Model.java
@@ -0,0 +1,8 @@
+package Model;
+
+import Model.MileStone3.test.Board;
+import Model.MileStone3.test.MyServer;
+
+public interface Model{
+    public void
+}
