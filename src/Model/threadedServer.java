5671df1ca2ddef71ebf5543ae45cd064c9438f5d
ALeph1 <alonmi0242@gmail.com>
2023-06-14 12:48:49 +0300

    Added thread support, changed model accordingly it's a draft..

---
+++ src/Model/threadedServer.java
@@ -0,0 +1,42 @@
+package Model;
+
+import Model.MileStone3.test.MyServer;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class threadedServer extends MyServer {
+    private final int MAX_PLAYERS = 4;
+    ExecutorService threadPool = Executors.newFixedThreadPool(MAX_PLAYERS);
+    public threadedServer(int port, multClientHandler ch) {
+        super(port, ch);
+        this.port = port;
+    }
+
+    private void startLogic(){ //The overloaded function
+
+        try {
+            ServerSocket server = new ServerSocket(port);
+            server.setSoTimeout(1000);
+
+            while (!stop) {
+                try {
+                    Socket aClient = server.accept();
+                    ch.handleClient(aClient.getInputStream(), aClient.getOutputStream()); // Might cause problems since the initialization of the streams might be incorrect by the time the thread turn will come.
+                    threadPool.execute((multClientHandler)ch);  // This is a server that assumes you will be using a multithreaded client handler (Which is an instance of Runnable)
+                    ch.close();
+                    aClient.close();
+                }catch (SocketTimeoutException e) {}
+
+            }
+            server.close();
+
+        }catch (IOException e){};
+
+    }
+
+}
